46c1199c3c1d924c762b57ca54ec71922e67cc5f
sakethravikanti <120639132+sakethravikanti@users.noreply.github.com>
2025-03-18 10:39:28 +0530

    Update pylint.sh

--- pylint.sh
+++ pylint.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail  
 
 # Define variables
-APP_DIR="/home/ubuntu/jenkins/jenkins/workspace/todo-pipeline_main/django-on-ec2"
+APP_DIR="/home/ubuntu/jenkins/jenkins/workspace/multi-branch_develop/django-on-ec2"
 PYTHON_BIN="/usr/bin/python3"
 
 # Navigate to the application directory
